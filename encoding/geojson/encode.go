379d18a1d9f0d74b638db67d0454681e6d842737
William MacKay <foobaz@gmail.com>
2014-08-28 18:03:07 -0700

    validate geojson more, return errors for invalid geometry

--- encoding/geojson/encode.go
+++ encoding/geojson/encode.go
@@ -2,8 +2,9 @@ package geojson
 
 import (
 	"encoding/json"
-	"github.com/foobaz/geom"
 	"reflect"
+
+	"github.com/foobaz/geom"
 )
 
 func ToGeoJSON(t geom.T) (interface{}, error) {
@@ -14,16 +15,41 @@ func ToGeoJSON(t geom.T) (interface{}, error) {
 			Coordinates: g,
 		}, nil
 	case geom.LineString:
+		err := validateLineString(g)
+		if err != nil {
+			return nil, err
+		}
+
 		return Geometry{
 			Type:        "LineString",
 			Coordinates: g,
 		}, nil
+	case geom.MultiLineString:
+		err := validateMultiLineString(g)
+		if err != nil {
+			return nil, err
+		}
+
+		return Geometry{
+			Type:        "MultiLineString",
+			Coordinates: g,
+		}, nil
 	case geom.Polygon:
+		err := validatePolygon(g)
+		if err != nil {
+			return nil, err
+		}
+
 		return Geometry{
 			Type:        "Polygon",
 			Coordinates: g,
 		}, nil
 	case geom.MultiPolygon:
+		err := validateMultiPolygon(g)
+		if err != nil {
+			return nil, err
+		}
+
 		return Geometry{
 			Type:        "MultiPolygon",
 			Coordinates: g,
@@ -75,4 +101,50 @@ func Encode(g geom.T) ([]byte, error) {
 	} else {
 		return nil, err
 	}
+}
+
+func validateLineString(l geom.LineString) error {
+	pointCount := len(l)
+	if pointCount < 2 {
+		return InsufficientPointsError{pointCount}
+	}
+
+	return nil
+}
+
+func validateMultiLineString(m geom.MultiLineString) error {
+	for _, l := range m {
+		err := validateLineString(l)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func validatePolygon(p geom.Polygon) error {
+	if len(p) == 0 {
+		return InsufficientPointsError{0}
+	}
+
+	for _, r := range p {
+		pointCount := len(r)
+		if pointCount < 4 {
+			return InsufficientPointsError{pointCount}
+		}
+	}
+
+	return nil
+}
+
+func validateMultiPolygon(m geom.MultiPolygon) error {
+	for _, p := range m {
+		err := validatePolygon(p)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
